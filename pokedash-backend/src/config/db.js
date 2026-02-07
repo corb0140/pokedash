f71cec48704ead08e7add31c977eaf4e67abcb41
corb0140 <corb0140@algonquinlive.com>
2026-02-07 15:08:50 -0500

    added docker

--- pokedash-backend/src/config/db.js
+++ pokedash-backend/src/config/db.js
@@ -3,11 +3,15 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: isProduction
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
 });
 
 module.exports = pool;
--- pokedash/src/data/audioTracks.ts
+++ pokedash/src/data/audioTracks.ts
@@ -1,9 +1,9 @@
-import track1 from '@/assets/audio/Pokémon Diamond, Pearl & Platinum - Champion Cynthia Battle Music (HQ) - Pokeli (youtube).mp3'
-import track2 from '@/assets/audio/Pokémon Diamond, Pearl & Platinum - Team Galactic Commander Battle Music (HQ) - Pokeli (youtube).mp3'
-import track3 from '@/assets/audio/Pokémon HeartGold & SoulSilver - Champion & Red Battle Music (HQ) - Pokeli (youtube).mp3'
-import track4 from '@/assets/audio/Pokémon HeartGold & SoulSilver - Gym Leader & Elite Four Battle Music (HQ) - Pokeli (youtube).mp3'
-import track5 from '@/assets/audio/Pokémon HeartGold & SoulSilver - Super Ancient Pokémon Battle Music (HQ) - Pokeli (youtube).mp3'
-import track6 from '@/assets/audio/Pokémon X_Y - Elite Four Battle Music (HQ) - Pokeli (youtube).mp3'
+import track1 from '@/assets/audio/Pokemon Diamond, Pearl & Platinum - Champion Cynthia Battle Music (HQ) - Pokeli (youtube).mp3'
+import track2 from '@/assets/audio/Pokemon Diamond, Pearl & Platinum - Team Galactic Commander Battle Music (HQ) - Pokeli (youtube).mp3'
+import track3 from '@/assets/audio/Pokemon HeartGold & SoulSilver - Champion & Red Battle Music (HQ) - Pokeli (youtube).mp3'
+import track4 from '@/assets/audio/Pokemon HeartGold & SoulSilver - Gym Leader & Elite Four Battle Music (HQ) - Pokeli (youtube).mp3'
+import track5 from '@/assets/audio/Pokemon HeartGold & SoulSilver - Super Ancient Pokemon Battle Music (HQ) - Pokeli (youtube).mp3'
+import track6 from '@/assets/audio/Pokemon X_Y - Elite Four Battle Music (HQ) - Pokeli (youtube).mp3'
 
 export const tracks = [
   {
@@ -23,7 +23,7 @@ export const tracks = [
     src: track4,
   },
   {
-    title: 'Super Ancient Pokémon Battle',
+    title: 'Super Ancient Pokemon Battle',
     src: track5,
   },
   {
